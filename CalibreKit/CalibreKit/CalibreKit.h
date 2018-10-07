dc65253ff3ae883756362954096bfed55617b793
Justin Marshall <jdmarshall1990@gmail.com>
2018-10-07 14:35:03 -0500

    Add initial Xcode project

---
+++ CalibreKit/CalibreKit/CalibreKit.h
@@ -0,0 +1,19 @@
+//
+//  CalibreKit.h
+//  CalibreKit
+//
+//  Created by Justin Marshall on 10/7/18.
+//  Copyright © 2018 Justin Marshall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CalibreKit.
+FOUNDATION_EXPORT double CalibreKitVersionNumber;
+
+//! Project version string for CalibreKit.
+FOUNDATION_EXPORT const unsigned char CalibreKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalibreKit/PublicHeader.h>
+
+
