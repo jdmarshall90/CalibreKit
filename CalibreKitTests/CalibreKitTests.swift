1c589661c5db22521277d3439ebcd087b71509ea
Justin Marshall <jdmarshall1990@gmail.com>
2018-10-07 21:03:13 -0500

    Cleanup

--- CalibreKitTests/CalibreKitTests.swift
+++ CalibreKitTests/CalibreKitTests.swift
@@ -10,17 +10,17 @@
 import XCTest
 
 class CalibreKitTests: XCTestCase {
-
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDown() {
         super.tearDown()
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -32,5 +32,4 @@ class CalibreKitTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
