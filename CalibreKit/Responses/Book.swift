620f90bfe544d6fb322092f692ed4f6e279ff183
Justin Marshall <jdmarshall1990@gmail.com>
2018-10-10 17:41:29 -0500

    Tighten up API for the Book struct

--- CalibreKit/Responses/Book.swift
+++ CalibreKit/Responses/Book.swift
@@ -8,25 +8,86 @@
 
 import Foundation
 
-// TODO: take this out
-// swiftlint:disable identifier_name
-
 // TODO: see if any / all of these need to be optional?
 
 public struct Book: ResponseSerializable {
-    public let application_id: Int
-    public let author_sort_map: [String: String] // TODO: subtype?
-    public let authors: [String]
+    
+    private enum CodingKeys: String, CodingKey {
+        case applicationID = "application_id"
+        case comments
+        case cover
+        case identifiers
+        case languages
+        case lastModified = "last_modified"
+        case tags
+        case thumbnail
+        case title
+        case titleSort = "title_sort"
+        case authors
+        case authorSortMap = "author_sort_map"
+    }
+    
+    public let applicationID: Int
+    public let authors: [Author]
     public let comments: String?
     public let cover: CoverEndpoint
-    
-//    public let identifiers: [String: [String: String]] // TODO: subtype, also this isn't working, come back to it
+    public let identifiers: [Identifier]
     public let languages: [String] // TODO: enum?
-    public let last_modified: String // TODO: date
+    public let lastModified: Date
     public let tags: [String]
     public let thumbnail: ThumbnailEndpoint
-    public let title: String
-    public let title_sort: String
+    public let title: Title
+    
+    public struct Author {
+        public let name: String
+        public let sortName: String
+    }
+    
+    public struct Identifier {
+        public let name: String // TODO: enum? Google, ISBN, etc.?
+        public let uniqueID: String
+    }
+    
+    public struct Title {
+        public let title: String
+        public let sortTitle: String
+    }
+    
+    private static let dateFormatter: ISO8601DateFormatter = {
+        let formatter = ISO8601DateFormatter()
+        formatter.timeZone = TimeZone.current // TODO: This doesn't seem to work, come back to this
+        return formatter
+    }()
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.applicationID = try container.decode(Int.self, forKey: .applicationID)
+        
+        let rawAuthors = try container.decode([String].self, forKey: .authors)
+        let rawAuthorSortMap = try container.decode([String: String].self, forKey: .authorSortMap)
+        self.authors = rawAuthors.map { Author(name: $0, sortName: rawAuthorSortMap[$0] ?? $0) }
+        
+        self.comments = try container.decode(Optional<String>.self, forKey: .comments)
+        self.cover = try container.decode(CoverEndpoint.self, forKey: .cover)
+        
+        let rawIdentifiers = try container.decode([String: String].self, forKey: .identifiers)
+        self.identifiers = rawIdentifiers.map { Identifier(name: $0.key, uniqueID: $0.value) }
+        
+        self.languages = try container.decode([String].self, forKey: .languages)
+        
+        let rawLastModified = try container.decode(String.self, forKey: .lastModified)
+        guard let lastModified = Book.dateFormatter.date(from: rawLastModified) else {
+            throw CalibreError.message("Unexected date format")
+        }
+        self.lastModified = lastModified
+        
+        self.tags = try container.decode([String].self, forKey: .tags)
+        
+        let rawTitle = try container.decode(String.self, forKey: .title)
+        let rawTitleSort = try container.decode(String.self, forKey: .titleSort)
+        self.title = Title(title: rawTitle, sortTitle: rawTitleSort)
+        
+        self.thumbnail = try container.decode(ThumbnailEndpoint.self, forKey: .thumbnail)
+    }
 }
-
-// swiftlint:enable identifier_name
