d68bbc306cb0f805b59fa84b438206eca96162f5
Justin Marshall <jdmarshall1990@gmail.com>
2019-01-25 21:44:09 -0600

    Add notes

--- CalibreKit/Responses/SetFields.swift
+++ CalibreKit/Responses/SetFields.swift
@@ -25,4 +25,54 @@ import Foundation
 
 public struct SetFields: ResponseSerializable {
     // TODO: Fill this in
+    /*
+{
+    "3": {
+    "#isbn": "9780061784811",
+    "author_sort": "Rollins, James",
+    "authors": [
+    "James Rollins"
+    ],
+    "comments": "<p>**</p>\\n<blockquote>\\n<p>In a masterful epic of timeless mystery and ripped-from-the-headlines scientific intrigue, <em>New York Times</em> bestselling author James Rollins takes mankind to its endpoint, to a fate written in rock and ice in an event known as The Sixth Extinction.</p>\\n</blockquote>\\n<p><strong>A remote military research station broadcasts a frantic distress call that ends with a chilling message: </strong>Kill us all**. When soldiers arrive to investigate, they discover everyone in the lab is dead\u2014not just the scientists, but every living thing for fifty square miles is annihilated: every animal, plant, and insect, even bacteria. The land is completely sterile\u2014and the blight is spreading.</p>\\n<p>To prevent the inevitable, Commander Gray Pierce and Sigma must decipher a threat that rises out of the distant past, a time when Antarctica was green and Earth\u2019s life balanced on a knife edge. Following clues from an ancient map rescued from the lost Library of Alexandria, Sigma will make a shocking discovery involving a prehistoric continent, and a new form of death buried under miles of ice.</p>\\n<p>From millennia-old secrets out of the frozen past to mysteries buried deep in the darkest jungles of today, Sigma will face its greatest challenge yet: stopping the coming extinction of mankind.</p>\\n<p>But is it already too late?</p>",
+    "format_sizes": {},
+    "formats": [],
+    "identifiers": {
+    "goodreads": "18679381",
+    "isbn": "9780061784811"
+    },
+    "lang_names": {
+    "eng": "English"
+    },
+    "languages": [
+    "eng"
+    ],
+    "last_modified": "2019-01-26T02:45:42.328022+00:00",
+    "pubdate": "2014-08-12T00:00:00+00:00",
+    "publisher": "HarperCollins Publishers",
+    "rating": 10,
+    "series": "Sigma Force",
+    "series_index": 10.0,
+    "sort": "6th Extinction, The",
+    "tags": [
+    "Action & Adventure",
+    "Fiction",
+    "Science",
+    "Suspense",
+    "Thrillers",
+    "History"
+    ],
+    "timestamp": "2018-10-04T22:44:36+00:00",
+    "title": "The 6th Extinction",
+    "urls_from_identifiers": [
+    [
+    "9780061784811",
+    "isbn",
+    "9780061784811",
+    "https://www.worldcat.org/isbn/9780061784811"
+    ]
+    ],
+    "uuid": "79f2c059-1d7c-4e1f-a276-8cef3b6bb0f0"
+    }
+    }
+    */
 }
